838a6033fc212b44a74424a4fcb557b4f083d343
Ari Olvera <badillo.ari@gmail.com>
2021-12-23 11:40:06 -0600

    feat: seccion 8

---
+++ Angular/03-paisesApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PaisModule,
+    SharedModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
