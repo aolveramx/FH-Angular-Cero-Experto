f78f87863bdf1d6a1e811ac2c169d0dc33e6d859
Ari Olvera <badillo.ari@gmail.com>
2022-11-01 17:52:15 -0600

    feat: seccion 17

--- Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent implements OnInit{
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre      : [, [ Validators.required, Validators.minLength(3) ]],
+    precio      : [, [ Validators.required, Validators.min(0) ]],
+    existencias : [, [ Validators.required, Validators.min(0) ]],
+  });
 
-  ngOnInit(): void {
+  constructor( private formBuilder: FormBuilder ) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 1600
+    })
+  }
+
+  campoValido( campo: string ) {
+    return (this.miFormulario.controls[campo].errors
+          && this.miFormulario.controls[campo].touched);
+  }
+
+  guardar() {
+    if( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
 }
--- Angular/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre:   ['', [ Validators.required, Validators.minLength(3) ]],
+    favoritos: this.formBuilder.array( [
+      [ 'Metal Gear', Validators.required ],
+      [ 'Ninja Gaiden', Validators.required ],
+    ], Validators.required )
+  });
+
+  nuevoFavorito: FormControl = this.formBuilder.control('', Validators.required);
+
+  get favoritosArray() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  campoValido( campo: string ) {
+    return (this.miFormulario.controls[campo].errors
+          && this.miFormulario.controls[campo].touched);
+  }
+
+  agregarFavorito() {
+    if( this.nuevoFavorito.invalid ) { return; }
+
+    //this.favoritosArray.push( new FormControl( this.nuevoFavorito.value, Validators.required ));
+    this.favoritosArray.push( this.formBuilder.control( this.nuevoFavorito.value, Validators.required ));
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrar( index: number ) {
+    this.favoritosArray.removeAt(index);
+  }
+
+  guardar() {
+    if( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
   }
 
 }
